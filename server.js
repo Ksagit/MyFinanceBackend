44ec782079de18beacf7a24855cd13b3949e898f
ksagit <kuba.kielaszek@gmail.com>
2025-05-23 17:23:21 +0200

    changed port

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const mongoose = require("mongoose");
 const cors = require("cors");
 
 const app = express();
-const port = 5001;
+const port = process.env.PORT || 5001;
 const mongoUri = process.env.MONGODB_URI;
 
 app.use(cors());
